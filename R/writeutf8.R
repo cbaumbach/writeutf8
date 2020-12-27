a1b91cbdf9204c493b51212ef58d18ae60b7aca8
Clemens Baumbach <clemens.baumbach@gmx.net>
2020-12-27 19:01:41 +0100

    Refactor

--- R/writeutf8.R
+++ R/writeutf8.R
@@ -1,9 +1,23 @@
 writeutf8 <- function(df, filename) {
+    write_without_reencoding(enc2utf8(df_to_text(df)), filename)
+}
+
+df_to_text <- function(df) {
+    c(collapse_header(df), collapse_columns(df))
+}
+
+collapse_header <- function(df) {
+    paste(names(df), collapse = "\t")
+}
+
+collapse_columns <- function(df) {
+    do.call(function(...) paste(..., sep = "\t"), unname(df[]))
+}
+
+write_without_reencoding <- function(text, filename) {
     con <- file(filename, open = "wb", encoding = "native.enc")
     on.exit(close(con))
-    header <- paste(names(df), collapse = "\t")
-    rows <- do.call(function(...) paste(..., sep = "\t"), unname(df[]))
-    writeLines(enc2utf8(c(header, rows)), con, sep = "\n", useBytes = TRUE)
+    writeLines(text, con, sep = "\n", useBytes = TRUE)
 }
 
 readutf8 <- function(filename, ...) {
