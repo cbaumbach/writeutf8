fc99c3d471739175663cdba337c79d6def627275
Clemens Baumbach <clemens.baumbach@gmx.net>
2020-12-27 18:40:46 +0100

    Use random data frames to find new test cases

--- tests/testthat/test-writeutf8.R
+++ tests/testthat/test-writeutf8.R
@@ -1,3 +1,12 @@
 test_that("integer columns work", {
     expect_read_equal_write(data.frame(x = 1:2, y = 3:4))
 })
+
+test_that("random data frames work", {
+    set.seed(1234)
+    for (i in 1:100) {
+        if (!expect_read_equal_write(random_data_frame())) {
+            break
+        }
+    }
+})
