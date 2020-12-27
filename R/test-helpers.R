ca74cf7af6a7aa76be954f5d78aa87938f2e0219
Clemens Baumbach <clemens.baumbach@gmx.net>
2020-12-27 18:39:15 +0100

    Make test case error message more informative

--- R/test-helpers.R
+++ R/test-helpers.R
@@ -8,9 +8,9 @@ expect_read_equal_write <- function(df) {
 expect_equal_df <- function(expected, actual) {
     if (!equal_df(expected, actual)) {
         fail(utils::capture.output(suppressWarnings({
-            cat(sprintf("\nGot %s:\n", describe_df(actual)))
+            cat(sprintf("\nGot %s\n\n", describe_df(actual)))
             print(actual)
-            cat(sprintf("\nWant %s:\n", describe_df(expected)))
+            cat(sprintf("\nWant %s\n\n", describe_df(expected)))
             print(expected)
         })))
         return(FALSE)
@@ -32,8 +32,10 @@ has_double <- function(df) {
 }
 
 describe_df <- function(df) {
-    sprintf("%dx%d table with %s", nrow(df), ncol(df),
-            paste(sapply(df, typeof), collapse = ", "))
+    sprintf("%dx%d table with %s and names of length %s",
+            nrow(df), ncol(df),
+            paste(sapply(df, typeof), collapse = ", "),
+            paste(nchar(names(df), type = "chars"), collapse = ", "))
 }
 
 test_random_vector <- function(random, type) {
